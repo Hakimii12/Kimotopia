6c885eb4bc9dc4a0267fba2affceba0e787a165c
Hakimii12 <117950633+Hakimii12@users.noreply.github.com>
2025-03-31 12:07:04 +0300

    $$$$$$$$$$

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,19 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { chakraProvider } from '@chakra-ui/react'
-import { Provider } from "@/components/ui/provider"
+import { extendTheme } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
+
+const theme = extendTheme({ colors })
 createRoot(document.getElementById('root')).render(
-  <Provider>
+  <ChakraProvider theme={thema}>
     <App />
-  </Provider>,
+  </ChakraProvider>,
 )
