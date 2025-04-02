6aec254bb84a7f3d3f57b8a590dc4e5a068a88ce
Hakimii12 <117950633+Hakimii12@users.noreply.github.com>
2025-04-02 16:05:37 +0300

    #######

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
+import {Button , HStack} from "@chakra-ui/react"
 function App() {
 
   return (
-    <>
-    
-    </>
+    <HStack>
+      <Button>Click me</Button>
+      <Button>Click me</Button>
+  </HStack>
   )
 }
 
