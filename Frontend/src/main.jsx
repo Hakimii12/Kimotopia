2c58528d7b95538983fc102a6330cb0b2d0646d5
Hakimii12 <117950633+Hakimii12@users.noreply.github.com>
2025-04-02 18:16:07 +0300

    ########

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,12 +1,38 @@
 import { Provider } from "@/components/ui/provider"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { mode } from '@chakra-ui/theme-tools'
+import {extendTheme} from "@chakra-ui/theme-utils";
+import { useColorMode } from "./components/ui/color-mode";
 import './index.css'
 import App from './App.jsx'
 
+const styles = {
+  global: (props) => ({
+    body: {
+      color: mode('gray.800', 'whiteAlpha.900')(props),
+      bg: mode('gray.100', '#101010')(props),
+    },
+  }),
+}
+
+const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: true,
+}
+
+const colors = {
+  gray: {
+    light: '#616161',
+    dark: '#1e1e1e',
+  },
+}
+
+const theme =extendTheme({ styles, config, colors })
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider>
+    <Provider theme={theme}>
       <App />
     </Provider>
   </StrictMode>,
