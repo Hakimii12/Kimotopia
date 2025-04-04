3f65b574db31a6636cba4cb30e7ea3af98c45e29
Hakimii12 <117950633+Hakimii12@users.noreply.github.com>
2025-04-05 01:17:51 +0300

    #####

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,17 @@
 import { useContext } from 'react'
 import { ContextProvider } from '../ContextApi/ContextApi'
 import { Route,Routes } from 'react-router-dom'
+import UserPage from '../pages/UserPage'
+import PostPage from '../pages/PostPage'
 function App() {
   return (
-    <Routes>
+    <div className="flex max-w-[620px] bg-black">
+      <Routes>
       <Route path='/:username' element={<UserPage/>}/>
       <Route path='/:username/post/:pid' element={<PostPage/>}/>
-
     </Routes>
+    </div>
+    
   )
 }
 
