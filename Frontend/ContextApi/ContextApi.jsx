29dfaac807ac6ee10d4b644eceac80c53295b52e
Hakimii12 <117950633+Hakimii12@users.noreply.github.com>
2025-04-05 01:23:26 +0300

    ####

--- Frontend/ContextApi/ContextApi.jsx
+++ Frontend/ContextApi/ContextApi.jsx
@@ -1,32 +1,10 @@
-import { extendTheme } from "@chakra-ui/theme-utils";
 import { createContext } from "react";
 export const ContextProvider=createContext()
 import React from 'react'
 function ContextApi({children}) {
-   const styles={
-        global:(props)=>{
-            return{
-                body:{
-                    color:mode('gray.800', 'whiteAlpha.900')(props),
-                    bg:mode('gray.100', '#101010')(props),
-                }
-            }
-        }
-   }
-   const config = {
-    initialColorMode: 'dark',
-    useSystemColorMode: false,
-  }
-  const colors = {
-    gray: {
-      light: '#616161',
-      dark: '#1e1e1e',
-    },
-  }
-  const theme =extendTheme({ styles, config, colors })
-  console.log(theme)
+   
   return (
-    <ContextProvider.Provider value={theme}>
+    <ContextProvider.Provider value={{}}>
         {children}
     </ContextProvider.Provider>
 
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,5 +1,4 @@
 import ContextApi from '../ContextApi/ContextApi'
-import { Provider } from "@/components/ui/provider"
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
@@ -7,9 +6,7 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <ContextApi>
     <BrowserRouter>  
-    <Provider>
       <App/>
-    </Provider>
     </BrowserRouter>
   </ContextApi>
 )
