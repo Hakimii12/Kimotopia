8e7f7a47781a21ae2a1d67695900ff3e9636da6a
Hakimii12 <117950633+Hakimii12@users.noreply.github.com>
2025-04-06 18:09:59 +0300

    ########

---
+++ Frontend/src/Components/Comment.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import profile from '../assets/zuck-avatar.png'
+import {FaEllipsisH } from 'react-icons/fa'
+import verified from "../assets/verified.png"
+import {BiLike, BiComment, BiShare, BiRepost } from 'react-icons/bi'
+import { ContextProvider } from '../../ContextApi/ContextApi'
+import { useContext } from 'react'
+function Comment() {
+      const {toggleLiked,
+        liked}=useContext(ContextProvider)
+  return (
+    <>
+        <div className=" mx-14 h-[1px] w-full bg-gray-300"></div>
+    <div className='mt-4 mb-4 mx-14 flex w-[99%]'>
+    <div className='w-[5%] flex flex-col items-center'>
+      <img src={profile} className='w-full rounded-full' />
+    </div>
+    <div className='flex flex-col w-[90%]'>
+      <div className="w-full flex h-fit justify-between">
+        <div className="ml-2 flex gap-1 items-start">
+          <h2 className='font-bold text-base'>markzukerberg</h2>
+          <div className='flex items-center h-full'>
+            <img src={verified} className='w-[15px] h-4' />
+          </div>
+        </div>
+        <div className='mr-2 flex gap-2 items-center'>
+          <p>1d</p>
+          <FaEllipsisH />
+        </div>
+      </div>
+      <p className='ml-2 sm:mt-3'>Lorem ipsum, dolor sit amet consectetur.</p>
+      <div className='h-full mt-4 w-full'>
+        <div className="w-[25%]  flex justify-between">
+          <BiLike size={17} onClick={(e)=>toggleLiked(e)} color={liked?`red`:``}/>
+          <BiComment size={16} />
+          <BiRepost size={17}/>
+          <BiShare size={17}/>
+        </div>
+        <div className='flex gap-2 mt-2 font-light text-sm'>
+            <span>801 likes</span>
+        </div>
+      </div>
+    </div>
+  </div>
+    </>
+
+  )
+}
+
+export default Comment
--- Frontend/src/Components/UserPostPage.jsx
+++ Frontend/src/Components/UserPostPage.jsx
@@ -6,6 +6,7 @@ import post1 from '../assets/post1.png'
 import {BiLike, BiComment, BiShare, BiRepost } from 'react-icons/bi'
 import { ContextProvider } from '../../ContextApi/ContextApi'
 import { useContext } from 'react'
+import Comment from './Comment'
 function UserPostPage() {
     const {toggleLiked,
         liked}=useContext(ContextProvider)
@@ -55,37 +56,12 @@ function UserPostPage() {
     </div>
     <div className=" mx-14 h-[1px] w-full bg-gray-300"></div>
   </div>
-  <div className='mt-4 mb-4 mx-14 flex w-[99%]'>
-    <div className='w-[5%] flex flex-col items-center'>
-      <img src={profile} className='w-full rounded-full' />
-    </div>
-    <div className='flex flex-col w-[90%]'>
-      <div className="w-full flex h-fit justify-between">
-        <div className="ml-2 flex gap-1 items-start">
-          <h2 className='font-bold text-base'>markzukerberg</h2>
-          <div className='flex items-center h-full'>
-            <img src={verified} className='w-[15px] h-4' />
-          </div>
-        </div>
-        <div className='mr-2 flex gap-2 items-center'>
-          <p>1d</p>
-          <FaEllipsisH />
-        </div>
-      </div>
-      <p className='ml-2 sm:mt-3'>Lorem ipsum, dolor sit amet consectetur.</p>
-      <div className='h-full mt-4 w-full'>
-        <div className="w-[25%]  flex justify-between">
-          <BiLike size={17} onClick={(e)=>toggleLiked(e)} color={liked?`red`:``}/>
-          <BiComment size={16} />
-          <BiRepost size={17}/>
-          <BiShare size={17}/>
-        </div>
-        <div className='flex gap-2 mt-2 font-light text-sm'>
-            <span>801 likes</span>
-        </div>
-      </div>
-    </div> 
-  </div>
+  <Comment/>
+  <Comment/>
+  <Comment/>
+  <Comment/>
+  <Comment/>
+  <Comment/>
   <div className=" mx-14 h-[1px] w-full bg-gray-300"></div>
     </div>
     
