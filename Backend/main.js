c6c5075b313ad8a68f3e62d662268a4cbf22416f
Hakimii12 <117950633+Hakimii12@users.noreply.github.com>
2025-04-07 13:58:31 +0300

    #########

--- Backend/main.js
+++ Backend/main.js
@@ -1,6 +1,9 @@
 import express from 'express'
+import dotenv from 'dotenv'
 const app=express()
 app.use(express.json())
-app.listen(5000,()=>{
-    console.log("server is running on port 500")
+dotenv.config()
+const port=process.env.PORT
+app.listen(port,()=>{
+    console.log(`server is running on port http://localhost:/${port}`)
 })
