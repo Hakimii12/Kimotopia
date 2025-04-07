b99c8aea43f7926dc29f270ac7b97a7159c81a83
Hakimii12 <117950633+Hakimii12@users.noreply.github.com>
2025-04-07 17:33:07 +0300

    #####

--- Backend/main.js
+++ Backend/main.js
@@ -2,12 +2,18 @@ import express from 'express'
 import dotenv from 'dotenv'
 import Database from './database/database.js'
 import userModel from './models/userModel.js'
+import postModel from './models/postModel.js'
+import cookieParser from 'cookie-parser'
 const app=express()
 app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
 //initializing database
 Database()
-//insitializing model
+//initializing model
 userModel
+postModel
+//intializing routes
 //app listining to the port
 dotenv.config()
 const port=process.env.PORT
--- Backend/models/postModel.js
+++ Backend/models/postModel.js
@@ -35,6 +35,8 @@ const postSchema=new mongoose.Schema({
             }
         }
     ]
+},{
+    timestamps:true
 })
 const Post =mongoose.model('Post',postSchema)
 export default Post
