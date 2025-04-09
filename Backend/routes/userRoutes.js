404f64257dd1611d9b6a9cf1d3876f1eec96a675
Hakimii12 <117950633+Hakimii12@users.noreply.github.com>
2025-04-09 12:16:33 +0300

    ######

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -2,15 +2,10 @@ import express from 'express'
 import {followUnfollow, getUser, login, logout, signUp, update} from '../controllers/Credential.js'
 import Authorization from '../middlewares/Authorization.js'
 const router=express.Router()
-
-
 router.post('/signUp',signUp)
 router.post('/login',login)
 router.post('/logout',logout)
 router.post('/followunfollow/:id',Authorization,followUnfollow)
 router.post('/update/:id',Authorization,update)
 router.get('/profile/:username',Authorization, getUser)
-
-
-
 export default router
