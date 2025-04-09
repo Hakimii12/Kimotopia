f5fa475900db32003e316f79ae622b3e57d04b68
Hakimii12 <117950633+Hakimii12@users.noreply.github.com>
2025-04-09 15:06:04 +0300

    ####

---
+++ Backend/controllers/PostControllers.js
@@ -0,0 +1,29 @@
+import Post from "../models/postModel.js"
+import User from "../models/userModel.js"
+export async function CreatePost(req,res){
+    try {
+      const {postedBy,text}=req.body
+        if(!postedBy || !text){
+            return res.status(400).json({message:"please poster and post text is required"})
+        }
+        if(text.length>500){
+            return res.status(400).json({message:"post should be less than 500 characters"})
+        }
+        const user =await User.findById(postedBy).select("-password")
+        if(!user){
+            return res.status(404).json({message:"user not found"})
+        }
+        if(user._id.toString() !==req.user._id.toString()){
+            return res.status(400).json({message:"you are not authorized to post"})
+        }
+        const newPost =new Post({
+            postedBy:postedBy,
+            text:text
+        })
+        await newPost.save()
+        res.status(200).json({message:"successfully creates post"})
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
+
+}
--- Backend/routes/postRoutes.js
+++ Backend/routes/postRoutes.js
@@ -1,3 +1,6 @@
 import express from "express"
+import { CreatePost } from "../controllers/PostControllers.js"
+import Authorization from "../middlewares/Authorization.js"
 const router=express.Router()
+router.post('/create',Authorization,CreatePost)
 export default router
