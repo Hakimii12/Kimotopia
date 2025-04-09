49a76af466d9bfc9414f50d6cdb3e23dc2f82ede
Hakimii12 <117950633+Hakimii12@users.noreply.github.com>
2025-04-09 15:59:42 +0300

    ####

--- Backend/controllers/PostControllers.js
+++ Backend/controllers/PostControllers.js
@@ -44,3 +44,26 @@ export async function GetPost(req,res) {
         res.status(500).json({message:error.message})
     }
 }
+export async function DeletePost(req,res){
+    try {
+        const {id}=req.params
+        if(!id){
+            return res.status(404).json({message:"no post"})
+        }
+        const post=await Post.findById(id)
+        if(!post){
+            return res.status(404).json({message:"post not found"})
+        }
+        const userPost = await User.findById(post.postedBy).select("-password")
+        if(!userPost){
+            return res.status(404).json({message:"user not founc"})
+        }
+        if(userPost._id.toString() !== req.user._id.toString()){
+            return res.status(400).json({message:"Unauthorized to delete this post"})
+        }
+        await Post.findByIdAndDelete(id)
+        return res.status(200).json({message:"successfully deleted the post"})
+    } catch (error) {
+        res.status(500).json({message:error.message}) 
+    }
+}
--- Backend/routes/postRoutes.js
+++ Backend/routes/postRoutes.js
@@ -1,7 +1,8 @@
 import express from "express"
-import { CreatePost, GetPost } from "../controllers/PostControllers.js"
+import { CreatePost, DeletePost, GetPost } from "../controllers/PostControllers.js"
 import Authorization from "../middlewares/Authorization.js"
 const router=express.Router()
 router.post('/create',Authorization,CreatePost)
 router.get('/getpost/:id',GetPost)
+router. delete('/deletepost/:id',Authorization,DeletePost)
 export default router
