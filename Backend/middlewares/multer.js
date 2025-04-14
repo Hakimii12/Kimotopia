1214b27e95e854422352261f4e9d62450edc1ad9
Hakimii12 <117950633+Hakimii12@users.noreply.github.com>
2025-04-14 16:37:55 +0300

    ##########

---
+++ Backend/middlewares/multer.js
@@ -0,0 +1,8 @@
+import multer from 'multer'
+const storage =multer.diskStorage({
+    filename: function (req,file,cb){
+        cb(null,file.fieldname+'_'+Date.now()+'_'+file.originalname)
+    }
+});
+const Upload =multer({storage:storage})
+export default Upload
--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import {followUnfollow, getUser, login, logout, signUp, update} from '../controllers/UserActivities.js'
 import Authorization from '../middlewares/Authorization.js'
+import Upload from '../middlewares/multer.js'
 const router=express.Router()
 router.post('/signUp',signUp)
 router.post('/login',login)
 router.post('/logout',logout)
 router.post('/followunfollow/:id',Authorization,followUnfollow)
-router.put('/update/:id',Authorization,update)
+router.put('/update/:id',Authorization,Upload.single('profilepic'),update)
 router.get('/profile/:username',Authorization, getUser)
 
 export default router
