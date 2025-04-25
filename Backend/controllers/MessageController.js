b4af44b925e2df950b068179e0ada4dde114446a
Hakimii12 <117950633+Hakimii12@users.noreply.github.com>
2025-04-25 15:20:41 +0300

    #######

--- Backend/controllers/MessageController.js
+++ Backend/controllers/MessageController.js
@@ -1,3 +1,21 @@
+import Conversation from "../models/Conversation.js";
+
 export async function CreateMessage(req,res){
-    
+    const {particepant,text}=req.body;
+    const sendId=req.user_id;
+    try {
+        const conversation= await Conversation.findOne({ participants: { $all: [sendId, particepant] } });
+        if(!conversation){
+            const newConversation=new Conversation({
+                particepants:[sendId,particepant]
+            });
+            await newConversation.save();
+        }
+        
+    } catch (error) {
+        res.status(500).json({message:error.message})
+        console.log(error.message)
+    }
+
 }
+
